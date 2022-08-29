81249ff83a4bc5e1bc10cbc815c6ee886d904c7f
Claudio Paz <claudio.paz.gonzalez@gmail.com>
2022-08-29 17:05:40 +0200

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :cars, only: [:index, :new, :create, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
