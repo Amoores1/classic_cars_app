037dd2e87e98ccf37b32b6a3f48d77190da1d446
ernesto baquero <ernest.baquero@gmail.com>
2022-08-30 15:43:24 +0100

    bug with carousel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Booking.destroy_all
+Car.destroy_all
+User.destroy_all
+user = User.new(email: "wow121212@email.com", password: "password123123", name: "Schumacher")
+user.save!
+car = Car.new(manufacturer: "ferrari", year: 2022, model: "F1", category:"F1", day_price: 1, user: user)
+car.save!
+Booking.create!(total_price: "2", start_date: "01/01/20", end_date: "12/12/22", user: user, car: car)
