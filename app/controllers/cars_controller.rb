a3c11ffb300f9905ee61e9c698c3a0250ef66794
hashim janjua <hashimj@hotmail.co.uk>
2022-09-02 11:27:35 +0100

    Added some crud actions and authentication

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -19,21 +19,32 @@ class CarsController < ApplicationController
     @car = Car.new
   end
 
-  # def create
-  #   @car = Car.new(car_params)
-  #   if @car.save
-  #     redirect_to car_path
-  #   else
-  #     render :new
-  #   end
-  # end
-
   def create
     @car = Car.new(car_params)
     # @car = Car.find(params[:car_id])
     @car.user = current_user
     @car.save!
     redirect_to cars_path
+  end
+
+  def yourcars
+    @cars = current_user.cars
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path, status: :see_other
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to cars_path(@car)
   end
 
   private
